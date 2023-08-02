6f64a133bfe48dfb281010dc2879bf8f9a614363
CHENiuuu <sanma0402@gmail.com>
2023-08-02 16:56:29 +0800

    test 新增與刪除資料

--- src/main/java/app/TestApp.java
+++ src/main/java/app/TestApp.java
@@ -2,6 +2,7 @@ package app;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 import core.util.HibernateUtil;
 import web.member.pojo.Member;
@@ -9,11 +10,53 @@ import web.member.pojo.Member;
 public class TestApp {
 	
 	public static void main(String[] args) {
+
+		//新增資料
+//		Member member = new Member();
+//		member.setUsername("使用者名稱");
+//		member.setPassword("密碼");
+//		member.setNickname("暱稱");
+//		
+//		TestApp app = new TestApp();
+//		app.insert(member);
+//		System.out.println(member.getId());
+		
+		//刪除資料
+		TestApp app = new TestApp();
+		System.out.println(app.deleteById(4));
+	}
+	
+	//新增資料
+//	public Integer insert(Member member) {
+//		SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+//		Session session = sessionFactory.openSession();
+//		try {
+//			Transaction transaction = session.beginTransaction();
+//			session.persist(member);
+//			transaction.commit();
+//			return member.getId();
+//		}catch (Exception e) {
+//			e.printStackTrace();
+//			session.getTransaction().rollback();
+//			return null;
+//		}
+//	}
+	
+	//刪除資料
+	public int deleteById(Integer id) {
 		SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
 		Session session = sessionFactory.openSession();
-		Member member = session.get(Member.class, 1);
-		System.out.println(member.getNickname());
-		HibernateUtil.shutdown();
+		try {
+			Transaction transaction = session.beginTransaction();
+			Member member = session.get(Member.class, id);
+			session.remove(member);
+			transaction.commit();
+			return member.getId();
+		}catch (Exception e) {
+			e.printStackTrace();
+			session.getTransaction().rollback();
+			return -1;  //基本型態的值不能為null，改成數值
+		}
 	}
 
 }
