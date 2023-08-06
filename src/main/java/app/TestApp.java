5bb06ea3937d7207a026d0fdf1d6b0c5d39c837f
CHENiuuu <sanma0402@gmail.com>
2023-08-06 09:07:56 +0800

    第13/14章

--- src/main/java/app/TestApp.java
+++ src/main/java/app/TestApp.java
@@ -1,10 +1,15 @@
 package app;
 
-import javax.persistence.Id;
+import java.util.List;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
 import core.util.HibernateUtil;
 import web.member.pojo.Member;
@@ -12,7 +17,31 @@ import web.member.pojo.Member;
 public class TestApp {
 	
 	public static void main(String[] args) {
-
+		
+		SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+		Session session = sessionFactory.openSession();
+		
+		CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
+		CriteriaQuery<Member> criteriaQuery = criteriaBuilder.createQuery(Member.class);
+		//From MEMBER
+		Root<Member> root = criteriaQuery.from(Member.class);
+		
+		//where USERNAME = ? and PASSWORD = ?
+		criteriaQuery.where(criteriaBuilder.and(
+				criteriaBuilder.equal(root.get("username"), "admin"),
+				criteriaBuilder.equal(root.get("password"), "P@ssw0rd")
+				));
+		
+		//select USERNAME, NICKNAME
+		criteriaQuery.multiselect(root.get("username"), root.get("nickname"));
+		
+		//order by ID
+		criteriaQuery.orderBy(criteriaBuilder.asc(root.get("id")));
+		
+		Member member = session.createQuery(criteriaQuery).uniqueResult();
+		System.out.println(member.getNickname());
+		//還沒理解======================================================================================
+				
 		//新增資料
 //		Member member = new Member();
 //		member.setUsername("使用者名稱");
@@ -37,8 +66,19 @@ public class TestApp {
 //		System.out.println(app.updateById(member));
 		
 		//查詢資料
-		TestApp app = new TestApp();
-		System.out.println(app.selectById(3).getNickname());
+//		TestApp app = new TestApp();
+//		System.out.println(app.selectById(3).getNickname());
+		
+		//查詢all
+//		TestApp app = new TestApp();
+//		app.selectAll().forEach(member -> System.out.println(member.getNickname()));
+		
+		//or
+		
+//		for (Member member : app.selectAll()) {
+//			System.out.println(member.getNickname());
+//		}
+		
 	}
 	
 //session在Hibernate裡面代表一個對資料庫的連線
@@ -108,21 +148,40 @@ public class TestApp {
 //		}
 //	}
 
-	//查詢資料
-	public Member selectById(Integer id) {
+//	//查詢資料
+//	public Member selectById(Integer id) {
+//		SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+//		Session session = sessionFactory.openSession();
+//		try {
+//			Transaction transaction = session.beginTransaction();
+//			Member member = session.get(Member.class, id);
+//			transaction.commit();
+//			return member;
+//		}catch (Exception e) {
+//			e.printStackTrace();
+//			session.getTransaction().rollback();
+//			return null;  //基本型態的值不能為null，改成任意數值
+//		}
+//	}
+
+	
+	//查詢
+	public List<Member> selectAll() {
 		SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
 		Session session = sessionFactory.openSession();
 		try {
 			Transaction transaction = session.beginTransaction();
-			Member member = session.get(Member.class, id);
+//			Query<Member> query = session.createQuery("FROM Member", Member.class);
+			Query<Member> query = session.createQuery(
+				"SELECT new web.member.pojo.member(username, nickname) FROM Member", Member.class);
+			List<Member> list = query.getResultList();
 			transaction.commit();
-			return member;
+			return list;
 		}catch (Exception e) {
 			e.printStackTrace();
 			session.getTransaction().rollback();
-			return null;  //基本型態的值不能為null，改成任意數值
+			return null;  
 		}
 	}
-
 	
 }
